bee146deaa44de4f1596e20256e0f672cf7fc7be
jhtan <jhtan@jhtan.com>
2015-02-12 02:03:46 -0400

    Changing the Facebook credentials.

--- app/config/packages/artdarek/oauth-4-laravel/config.php
+++ app/config/packages/artdarek/oauth-4-laravel/config.php
@@ -22,8 +22,8 @@ return array(
 		 * Facebook
 		 */
         'Facebook' => array(
-            'client_id'     => '391601977588709',
-            'client_secret' => 'f20f76fc6a56ef90e81ea77f4ca1f47c',
+            'client_id'     => '1547130082232999',
+            'client_secret' => '8f1a8f57b9e0c4e67a702672b9f4a55b',
             'scope'         => array('email','read_friendlists','user_online_presence'),
         ),		
 
