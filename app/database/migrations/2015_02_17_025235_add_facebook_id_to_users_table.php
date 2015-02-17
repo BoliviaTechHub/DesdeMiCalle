b067cbdeb381275b84c75b0df674f6abdf840ecd
jhtan <jhtan@jhtan.com>
2015-02-17 15:10:04 -0400

    Saving the user data of facebook in our db.

--- app/database/migrations/2015_02_17_025235_add_facebook_id_to_users_table.php
+++ app/database/migrations/2015_02_17_025235_add_facebook_id_to_users_table.php
@@ -14,7 +14,7 @@ class AddFacebookIdToUsersTable extends Migration {
 	{
 		Schema::table('users', function(Blueprint $table)
 		{
-            $table->string('facebook_id')->nullable();
+      $table->string('facebook_id');
 		});
 	}
 
@@ -27,7 +27,7 @@ class AddFacebookIdToUsersTable extends Migration {
 	{
 		Schema::table('users', function(Blueprint $table)
 		{
-            $table->dropColumn('facebook_id');
+      $table->dropColumn('facebook_id');
 		});
 	}
 
