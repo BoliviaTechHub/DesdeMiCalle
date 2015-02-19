bf1ade97e251a651f7517c23078bd0aaae09589d
jhtan <jhtan@jhtan.com>
2015-02-18 20:54:09 -0400

    Adding the facebook_id column to the users table, again.. :/ .

---
+++ app/database/migrations/2015_02_19_004232_add_facebook_id_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFacebookIdToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->string('facebook_id')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+            $table->dropColumn('facebook_id');
+		});
+	}
+
+}
