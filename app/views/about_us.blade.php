97aacafffa192c96ea5ec1df6e73667b99c71289
jhtan <jhtan@jhtan.com>
2015-05-27 10:38:54 -0400

    About us correction..

--- app/views/about_us.blade.php
+++ app/views/about_us.blade.php
@@ -13,11 +13,20 @@
             </p>
             <h3 class="titulo">¿Qué es HIVOS?</h3>
             <p>
-                Desde mi calle es una plataforma de código abierto, esto significa que cualquiera puede tomarla y adaptarla para las necesidades de la ciudad en la que se quiera implementar. Todo esto de forma gratuita. Esperamos que el proyecto aporte nuevas funcionalidades al software, para que sean utilizadas en otras partes del mundo.
+                Hivos es una organización internacional que busca nuevas soluciones a los problemas globales persistentes. Con proyectos inteligentes en los lugares correctos, nos oponemos a la discriminación, la desigualdad, el abuso de poder y el uso no sostenible de los recursos de nuestro planeta. Nuestro principal objetivo es lograr el cambio estructural. Es por ello que colaboramos con empresas innovadoras, los ciudadanos y sus organizaciones. Compartimos un sueño con ellos de las economías sostenibles y sociedades inclusivas.
             </p>
-            <h3 class="titulo">¿Qué es SIM?</h3>
             <p>
-                Desde mi calle es una plataforma de código abierto, esto significa que cualquiera puede tomarla y adaptarla para las necesidades de la ciudad en la que se quiera implementar. Todo esto de forma gratuita. Esperamos que el proyecto aporte nuevas funcionalidades al software, para que sean utilizadas en otras partes del mundo.
+                Creemos que todo ser humano tiene derecho a vivir en libertad y dignidad. Puedes ser quien eres, si eres hombre, mujer, gay, heterosexual o cualquier otra cosa. Además, usted debe tener la libertad de creer y decir lo que quieras, para desafiar a los gobiernos y agitar el orden establecido. Tu libertad sólo termina donde comienza la libertad de otra persona, y por supuesto donde la tierra impone sus límites. El agotamiento de los recursos naturales, la pérdida de biodiversidad y el cambio climático ya tiene consecuencias importantes para los habitantes más vulnerables de nuestro planeta. Ellos, y las generaciones futuras, tienen derecho a un mundo verdaderamente sostenible.
+            </p>
+            <h3 class="titulo">¿Qué es ACM - SIM?</h3>
+            <p>
+                ACM - SIM es una Asociacion Civil sin fines de lucro establecida legalmente en La Paz, Bolivia.
+            </p>
+            <p>
+                Sus actividades se enfocan en la inclusion tecnologica, formacion y competitividad a nivel de TIC. Promueve la difusion e implementacion de los datos y gobierno abierto a nivel nacional.
+            </p>
+            <p>
+                Como grupo de trabajo ACM - SIM tiene 10 años de experiencia en estas actividades, habiendo creado (o participado en su creacion) de las competencias TIC mas importantes de Bolivia, pero legalmente somos una ONG con 1 año de antiguedad.
             </p>
         </div>
     </div>
