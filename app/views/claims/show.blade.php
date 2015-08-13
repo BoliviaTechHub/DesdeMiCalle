cec1f7abd7cdc8b55e67386059be74b40030fb4e
jhtan <jhtan@jhtan.com>
2015-08-13 04:11:01 -0400

    facebook test..

--- app/views/claims/show.blade.php
+++ app/views/claims/show.blade.php
@@ -35,7 +35,7 @@
                         <a id="fb-share"
                            style='text-decoration:none;'
                            type="icon_link"
-                           onClick="window.open('http://www.facebook.com/sharer.php?s=100&p[title]={{$claim->title}}&p[summary]={{substr($claim->description, 0, 30) . '...'}}&p[url]={{URL::to('/');}}/claims/{{$claim->id}}&p[images][0]={{$claim->image_url}}','sharer','toolbar=0,status=0,width=580,height=325');"
+                           onClick="window.open('http://www.facebook.com/sharer.php?s=100&p[title]={{urlencode($claim->title)}}&p[summary]={{urlencode(substr($claim->description, 0, 30) . '...')}}&p[url]={{URL::to('/');}}/claims/{{$claim->id}}&p[images][0]={{urlencode($claim->image_url)}}','sharer','toolbar=0,status=0,width=580,height=325');"
                            href="javascript: void(0)">
                             <i class="icon-facebook2 icon-medium"></i>
                         </a>
